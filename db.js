d0ea069c416b25e7e9eebbc7be998015e80ef3d0
Asura <u202020239@upc.edu.pe>
2022-11-05 20:00:27 -0500

    Initial commit

---
+++ db.js
@@ -0,0 +1,33 @@
+const parkings = require("./data/parkings.json");
+const history = require("./data/History.json");
+
+
+module.exports = {
+  parkings,
+  history
+}
+
+
+// const items = require("./data/items");
+
+// module.exports = () => {
+
+//   var tags = items.map((i) => i.tags);
+//   tags = [].concat.apply([], tags);
+//   tags = [...new Set(tags)];
+
+ 
+
+//   var brands = items.map((i) => i.manufacturer);
+//   brands = [...new Set(brands)];
+
+//   var itemTypes = items.map((i) => i.itemType);
+//   itemTypes = [...new Set(itemTypes)];
+
+//   return {
+//     items,
+//     tags,
+//     brands,
+//     itemTypes,
+//   };
+// };
